b438010dab08bd8afe578b556ceb9b4e6d36c50f
AAUDI <audiahmad@hotmail.com>
2023-11-11 10:27:09 +0200

    binding TDR in progress

--- src/TDR/pymodTDR.cc
+++ src/TDR/pymodTDR.cc
@@ -2,10 +2,9 @@
 #include <pybind11/stl.h>
 
 namespace py = pybind11;
-void export_modCT(py::module &);
 
-
-PYBIND11_MODULE(pyDICOS, m)
+void export_tdr(py::module &);
+void export_modTDR(py::module &m)
 {
-   export_modCT(m);
+    export_tdr(m);
 }
