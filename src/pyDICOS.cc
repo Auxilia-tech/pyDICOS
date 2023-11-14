735143c42abf0e16ca01711f68f25459ede74514
AAUDI <audiahmad@hotmail.com>
2023-11-14 15:21:41 +0200

    binding the filesystem class in progress

--- src/pyDICOS.cc
+++ src/pyDICOS.cc
@@ -1,14 +1,9 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
 #include "headers.hh"
-
-namespace py = pybind11;
-//void export_modDX(py::module &);
-//void export_modTDR(py::module &);
 
 PYBIND11_MODULE(pyDICOS, m)
 {
+   export_FS(m);
    export_CT(m);
-   //export_modDX(m);
-   //export_modTDR(m);
+   //export_DX(m);
+   //export_TDR(m);
 }
