4e98b5aaebc615595947c8014ad80a3c805955f1
AAUDI <audiahmad@hotmail.com>
2023-11-14 19:39:46 +0200

    binding filename class and error log class

--- python/test_CT_read.py
+++ python/test_CT_read.py
@@ -1,7 +1,9 @@
-#from pyDICOS import FS
+from pyDICOS import Filename
 from pyDICOS import CT
 
 CTObject = CT()
-
+FS = Filename("SimpleCT0000.dcs")
+print(FS.GetExtension())
+#success = CTObject.Read()
 
 
