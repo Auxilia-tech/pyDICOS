6646655e3c9cf844bd793df499c67e9420de4a23
AAUDI <audiahmad@hotmail.com>
2023-11-14 19:39:46 +0200

    binding filename class and error log class

---
+++ src/ErrorLog/pyErrorLog.cc
@@ -0,0 +1,21 @@
+#include "../headers.hh"
+
+#include "SDICOS/ErrorLog.h"
+
+using namespace SDICOS;
+
+void export_ERRORLOG(py::module &m)
+{
+    py::enum_<Redirect::REDIRECT_LOG_TYPE>(m, "REDIRECT_LOG_TYPE")
+        .value("enumRedirect_Warning", Redirect::REDIRECT_LOG_TYPE::enumRedirect_Warning)
+        .value("enumRedirect_Error", Redirect::REDIRECT_LOG_TYPE::enumRedirect_Error);
+
+    py::class_<ErrorLog>(m, "SDICOS::ErrorLog")
+        .def(py::init<>())
+        .def(py::init<const ErrorLog&>(), py::arg("errorlog"))
+        .def("FreeMemory", &ErrorLog::FreeMemory)
+        .def_static("SetErrorLogRedirect", &ErrorLog::SetErrorLogRedirect, "Set the redirect function")
+        .def_static("HasRedirect", &ErrorLog::HasRedirect)
+        .def("IgnoreErrorLogRedirect", &ErrorLog::IgnoreErrorLogRedirect)
+        .def("UseErrorLogRedirect", &ErrorLog::UseErrorLogRedirect);
+}
--- src/pyDICOS.cc
+++ src/pyDICOS.cc
@@ -2,6 +2,7 @@
 
 PYBIND11_MODULE(pyDICOS, m)
 {
+   export_ERRORLOG(m);
    export_FS(m);
    export_CT(m);
    //export_DX(m);
