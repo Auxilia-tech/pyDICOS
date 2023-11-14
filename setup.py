e11b064ff8f45ad31aa735f8ca748ac140f21d74
Louis Combaldieu <louis.combaldieu@auxilia-tech.com>
2023-11-14 12:51:13 +0100

    First release setup commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name='pydicos',
+    packages=['pydicos'],
+    version='0.1.0',
+    license='MIT',
+    description='TYPE YOUR DESCRIPTION HERE',
+    author='Louis Combaldieu',
+    author_email='louis.combaldieu@auxilia-tech.com',
+    url='https://github.com/Auxilia-tech/pydicos',
+    download_url='https://github.com/Auxilia-tech/pydicos/',
+    keywords=['DICOS', 'dicos', 'Digital', 'Imaging', 'Communications', 'Security'],
+    install_requires=[
+        'numpy',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ],
+)
