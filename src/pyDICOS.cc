788777a2eac3b5f761891b05d48f96c7b0f92ab3
AAUDI <audiahmad@hotmail.com>
2023-11-25 15:08:26 +0200

    create a simple CT object in progress

--- src/pyDICOS.cc
+++ src/pyDICOS.cc
@@ -5,6 +5,7 @@ PYBIND11_MODULE(pyDICOS, m)
    export_IMEMMANAGER(m);
    export_ERRORLOG(m);
    export_FS(m);
+   export_SECTION(m);
    export_CT(m);
    export_DCSSTRING(m);
    export_MEMORYBUFFER(m);
