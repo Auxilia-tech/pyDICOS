18166da2a011b2f0f6c8c5ee3d66855d82b5c03b
AAUDI <audiahmad@hotmail.com>
2023-11-27 08:49:38 +0200

    bind new methods of section needed to create a CT object

--- src/Vector3D/pyVector3D.hh
+++ src/Vector3D/pyVector3D.hh
@@ -12,8 +12,10 @@ void export_VECTOR3D(py::module &m,  const std::string & typestr) {
     std::string pyclass_point3d_name = std::string("Point3D") + typestr;
     py::class_<Point3D<T>>(m, pyclass_point3d_name.c_str());
     py::class_<Vector3D<T>, Point3D<T>>(m, pyclass_vect3d_name.c_str())
-    .def(py::init<>())
-    .def(py::init<T, T, T>(), py::arg("cx") = 0, py::arg("cy") = 0, py::arg("cz") = 0)
-    .def(py::init<const T *>())
-    .def(py::init<const Point3D<T> &>());
+        .def(py::init<T, T, T>(), py::arg("cx") = 0, py::arg("cy") = 0, py::arg("cz") = 0)
+        .def(py::init<const T *>())
+        .def(py::init<const Point3D<T> &>())
+        .def("__copy__", [](const Point3D<T> &self) { return Point3D<T>(self); })
+        .def(py::self == py::self)
+        .def(py::self != py::self);
 }
