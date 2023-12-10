8618e1b670ed4e46a1b4edb15a5793a1722c24c6
AAUDI <audiahmad@hotmail.com>
2023-12-10 09:28:51 +0200

    Binding DX class in progress, binding Image2D class, Write DX Object in progress

--- python/test_DX_write.py
+++ python/test_DX_write.py
@@ -3,15 +3,15 @@ from pyDICOS import Filename
 from pyDICOS import ErrorLog
 from pyDICOS import DX
 from pyDICOS import CT
-from pyDICOS import Array1DS_UINT16
-from pyDICOS import Array1DPairBoolMemBuff
-from pyDICOS import Array3DLargeS_UINT16
+from pyDICOS import Vector3Dfloat
+from pyDICOS import Point3Dfloat
+from pyDICOS import Array2DS_UINT16
 from pyDICOS import Volume
 import numpy as np
 
 
 
-def CreateDXForProcessingSimple() :
+def CreateDXForProcessingSimple():
     
     DXObject =  DX(CT.OBJECT_OF_INSPECTION_TYPE.enumTypeCargo,
                 DX.PRESENTATION_INTENT_TYPE.enumProcessing,
@@ -21,6 +21,24 @@ def CreateDXForProcessingSimple() :
     width = 256 
     height = 128
 
-    SDICOS::Image2D& dxData = dx.GetXRayData();
-        dxData.Allocate(SDICOS::Image2D::enumUnsigned16Bit, width,height)
+    dxData = DXObject.GetXRayData()
+    dxData.Allocate(Volume.IMAGE_DATA_TYPE.enumUnsigned16Bit, width, height)
+    rawData = dxData.GetUnsigned16()
+
+    count = 0
+
+    for row in range(dxData.GetHeight()):
+        for col in range(dxData.GetWidth()):
+            rawData.Set(col, row, count)
+            count = count + 1
  
+    DXObject.SetKVP(1)
+    DXObject.SetImageOrientation(Vector3Dfloat(1, 0, 0), Vector3Dfloat(0, 1, 0))
+    DXObject.SetImagePosition(Point3Dfloat(0, 0, 1))
+    DXObject.SetXRayTubeCurrent(2.5)
+
+def main():
+    CreateDXForProcessingSimple()
+
+if __name__ == "__main__":
+    main()
