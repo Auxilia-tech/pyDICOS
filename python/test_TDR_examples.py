a0ea2758b12f76a5e7bb9338a36dd89b93ccc9ca
AAUDI <audiahmad@hotmail.com>
2023-12-14 21:40:13 +0200

    Binding TDR class in progress - start to create examples in python for testing

---
+++ python/test_TDR_examples.py
@@ -0,0 +1,28 @@
+from pyDICOS import Filename
+from pyDICOS import ErrorLog
+from pyDICOS import CustomMemoryManager
+from pyDICOS import Section
+from pyDICOS import TDR
+from pyDICOS import Folder
+from pyDICOS import DcsString, DcsLongString
+from pyDICOS import MemoryBuffer
+from pyDICOS import Array1DS_UINT16
+from pyDICOS import Array1DArray2DS_UINT16
+from pyDICOS import Array1DPairBoolMemBuff
+from pyDICOS import Array1DPairFilenameErrorLog
+from pyDICOS import Array2DS_UINT16
+from pyDICOS import Vector3Dfloat
+from pyDICOS import Volume
+from pyDICOS import Array3DLargeS_UINT16
+from pyDICOS import Point3DS_UINT16
+from pyDICOS import DicosFileListing
+
+import numpy as np
+
+def CreateNoThreatTDRForBaggageSimple():
+    TDRInstanceNumber = 1234
+
+def main():
+    CreateNoThreatTDRForBaggageSimple()
+if __name__ == "__main__":
+    main()
