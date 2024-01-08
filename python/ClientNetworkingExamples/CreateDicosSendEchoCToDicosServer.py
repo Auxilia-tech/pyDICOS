ae19cb3b4a905fc59c316fdc4e797e422de16472
AAUDI <audiahmad@hotmail.com>
2024-01-09 00:23:29 +0200

    binding examples related to the server and networking is in progress

---
+++ python/ClientNetworkingExamples/CreateDicosSendEchoCToDicosServer.py
@@ -0,0 +1,24 @@
+from pyDICOS import DcsClient
+from pyDICOS import DcsString
+from pyDICOS import Filename
+from pyDICOS import DcsApplicationEntity
+
+def main():
+    client = DcsClient()
+    client.SetServerPortandIP(1000, DcsString("1.1.1.1"))
+    client.SetSourceApplication(DcsApplicationEntity("ClientExample"))
+    client.SetDestinationApplication(DcsApplicationEntity("Server"))
+    if client.Echo() :
+        print("Echo success")
+    else:
+        print("Echo failed")
+        print("Error log:")
+        print(client.GetErrorLog())
+ 
+        client.GetErrorLog().WriteLog(Filename("ErrorLog - Echo Failed.txt"))
+        return 1
+    return 0
+
+if __name__ == "__main__":
+    main()
+    
