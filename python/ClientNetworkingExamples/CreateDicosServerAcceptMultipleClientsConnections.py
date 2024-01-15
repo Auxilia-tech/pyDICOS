fe28940d243aa2bdf92c28d4a78977a1c7ab9144
AAUDI <audiahmad@hotmail.com>
2024-01-15 12:46:09 +0200

    binding IrecDcsServer and IDcsServer)

--- python/ClientNetworkingExamples/CreateDicosServerAcceptMultipleClientsConnections.py
+++ python/ClientNetworkingExamples/CreateDicosServerAcceptMultipleClientsConnections.py
@@ -5,20 +5,7 @@ from pyDICOS import DcsApplicationEntity
 
 
 def main():
-    client = DcsClient()
-    client.SetServerPortandIP(1000, DcsString("1.1.1.1"))
-    client.SetSourceApplication(DcsApplicationEntity("ClientExample"))
-    client.SetDestinationApplication(DcsApplicationEntity("Server"))
-    if client.Echo() :
-        print("Echo success")
-    else:
-        print("Echo failed")
-        print("Error log:")
-        print(client.GetErrorLog().GetErrorLog().Get())
-        client.GetErrorLog().WriteLog(Filename("ErrorLog - Echo Failed.txt"))
-        return 1
-    return 0
-
+   test = 1
 if __name__ == "__main__":
     main()
     
---
+++ src/DcsServer/pyDcsServer.cc
@@ -0,0 +1,54 @@
+#include "../headers.hh"
+
+#include "SDICOS/Host.h"
+
+using namespace SDICOS;
+
+void export_DCSSERVER(py::module &m)
+{
+   py::class_<Network::IDcsServer>(m, "IDcsServer");
+   py::class_<Network::DcsServer, Network::IDcsServer>(m, "DcsServer")
+      .def(py::init<>())
+      .def("SetReadTimeoutInMilliseconds", &Network::DcsServer::SetReadTimeoutInMilliseconds, py::arg("nTimeoutMilliseconds") = 1000)
+      .def("GetReadTimeoutInMilliseconds", &Network::DcsServer::GetReadTimeoutInMilliseconds)
+      .def("SetWriteTimeoutInMilliseconds", &Network::DcsServer::SetWriteTimeoutInMilliseconds, py::arg("nTimeoutMilliseconds")  = 1000)
+      .def("GetWriteTimeoutInMilliseconds", &Network::DcsServer::GetWriteTimeoutInMilliseconds)
+      .def("SetIP", &Network::DcsServer::SetIP, py::arg("ds"))
+      .def("GetIP", &Network::DcsServer::GetIP)
+      .def("SetPort", &Network::DcsServer::SetPort, py::arg("nPort"))
+      .def("GetPort", &Network::DcsServer::GetPort)
+      .def("SetApplicationName", &Network::DcsServer::SetApplicationName, py::arg("ae"))
+      .def("GetApplicationName", &Network::DcsServer::GetApplicationName)
+      .def("RequireApplicationNames", &Network::DcsServer::RequireApplicationNames)
+      .def("IgnoreApplicationNames", &Network::DcsServer::IgnoreApplicationNames)
+      .def("IsConnectionFromApplicationEnabled", &Network::DcsServer::IsConnectionFromApplicationEnabled, py::arg("ae"))
+      .def("SetSslCertificate", py::overload_cast<const DcsString&, const DcsString&, const DcsString&, ErrorLog&>(&Network::DcsServer::SetSslCertificate), 
+                                  py::arg("dsFilenamePFX"), 
+                                  py::arg("dsPassword"),
+                                  py::arg("dsCN"),
+                                  py::arg("errorlog"))
+      .def("SetSslCertificate", py::overload_cast<const DcsString&, const DcsString&, ErrorLog&>(&Network::DcsServer::SetSslCertificate), 
+                                  py::arg("dsFilenamePFX"), 
+                                  py::arg("dsPassword"),
+                                  py::arg("errorlog"))
+      .def("IsUsingSsl", &Network::DcsServer::IsUsingSsl)
+      .def("SetTLSClientCertificateRequired", &Network::DcsServer::SetTLSClientCertificateRequired, py::arg("bRequired"))
+      .def("IsTLSClientCertificateRequired", &Network::DcsServer::IsTLSClientCertificateRequired)
+      .def("SetSslAllowedCiphers", &Network::DcsServer::SetSslAllowedCiphers, py::arg("dsAllowedCiphers"))
+      .def("GetSslAllowedCiphers", &Network::DcsServer::GetSslAllowedCiphers)
+      .def("GetTlsCipherSuite", &Network::DcsServer::GetTlsCipherSuite)
+      .def("AllowAllCients", &Network::DcsServer::AllowAllCients)
+      .def("RequireUserNameFromClient", &Network::DcsServer::RequireUserNameFromClient)
+      .def("RequireUserNameAndPasscodeFromClient", &Network::DcsServer::RequireUserNameAndPasscodeFromClient)
+      .def("IsValidUserNameAndPasscode", &Network::DcsServer::IsValidUserNameAndPasscode, py::arg("dsUserName"), py::arg("dsPasscode"))
+      .def("IsValidUserName", &Network::DcsServer::IsValidUserName, py::arg("dsUserName"))
+      .def("DisableDataCompression", &Network::DcsServer::DisableDataCompression)
+      .def("IsDataCompressionDisabled", &Network::DcsServer::IsDataCompressionDisabled)
+      .def("UpdateCallback", py::overload_cast<Network::IReceiveCallback&>(&Network::DcsServer::UpdateCallback), 
+                                  py::arg("icallback"))
+      .def("UpdateCallback", py::overload_cast<const Network::IDcsServer::RETRIEVE_METHOD>(&Network::DcsServer::UpdateCallback), 
+                                  py::arg("nPreferAPI"));
+
+      
+      
+}
