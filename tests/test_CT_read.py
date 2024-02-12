ec5c28a13c54cfbd15ececaae1644b5c1d0e2149
Louis Combaldieu <louis.combaldieu@auxilia-tech.com>
2024-02-12 19:18:12 +0100

    :sparkles: Changed code structure

--- tests/test_CT_read.py
+++ tests/test_CT_read.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pytest
 
-from pydicos import CTLoader
+from pydicos import read_dcs
 
 
 @pytest.mark.order(after="tests/test_CT_write.py::test_create_ct_files")
 def test_loading_from_file():
     # Example 1: Load CT from a file
-    ct_object = CTLoader(filename="SimpleCT/SimpleCT0000.dcs")
+    ct_object = read_dcs(filename="SimpleCT/SimpleCT0000.dcs", dcs_type="CT")
     data = ct_object.get_data()
     assert len(data) == 1
     assert data[0].shape == (40, 20, 10)
