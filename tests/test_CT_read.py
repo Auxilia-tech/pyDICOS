2fc3d923f4ab87c8375bea60a49c47cce837d61a
Louis Combaldieu <louis.combaldieu@auxilia-tech.com>
2024-11-28 10:26:34 +0100

    ✅ added pathlib test case, bumped version

--- tests/test_CT_read.py
+++ tests/test_CT_read.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pytest
-
+from pathlib import Path
 from pydicos import dcsread, dcswrite, CTLoader
 from tests.test_utils import get_tdr_data_output_template, get_pto_data, set_alarm_decision
 
 
 @pytest.mark.order(after="tests/test_CT_write.py::test_create_ct_files")
 def test_loading_from_file():
-    ct_object = dcsread("SimpleCT/SimpleCT0000.dcs")
+    ct_object = dcsread(Path("SimpleCT", "SimpleCT0000.dcs"))
     data = ct_object.get_data()
     assert len(data) == 1
     assert data[0].shape == (40, 20, 10)
@@ -15,12 +15,12 @@ def test_loading_from_file():
 
 @pytest.mark.order(after="tests/test_CT_write.py::test_create_ct_files")
 def test_loading_from_file_in_place():
-    ct_object = dcsread("SimpleCT/SimpleCT0000.dcs")
+    ct_object = dcsread(Path("SimpleCT", "SimpleCT0000.dcs"))
     data = ct_object.get_data()
     assert len(data) == 1
     assert data[0].shape == (40, 20, 10)
     assert np.all(data[0] == 48879)
-    dcswrite(ct_object, "SimpleCT/SimpleCT0000_2.dcs")
+    dcswrite(ct_object, Path("SimpleCT", "SimpleCT0000_2.dcs"))
 
     ct_object_2 = dcsread("SimpleCT/SimpleCT0000_2.dcs")
     data2 = ct_object_2.get_data()
@@ -45,7 +45,6 @@ def test_generate_tdr():
     tdr_data["PTOs"].append(get_pto_data(1, [40, 40, 40], [5, 5, 5], f"Label1", 0.7, np.ones((5, 5, 5)).astype(np.bool_).astype(np.uint8)))
     set_alarm_decision(tdr_data)
     tdr_object = ct_object.generate_tdr(tdr_data, output_file = "CTwithTDR/TDR2.dcs")
-    print(tdr_object.get_data())
     assert tdr_object.get_data()["PTOs"][0]["Assessment"]["probability"] == pytest.approx(0.7, 1e-6)
     assert tdr_object.get_data()["AlarmDecision"] == 1
     assert np.sum(tdr_object.get_data()["PTOs"][0]["Bitmap"]) == np.sum(np.ones((5, 5, 5)).astype(np.bool_).astype(np.uint8))
