4e0fbcefe01904ccf7dd2f4331e77a2720465269
LounesAl <lounas.allioui@hotmail.fr>
2025-02-13 11:35:52 +0100

    FIX py::overload_cast

--- src/IReceiveCallback/pyDataProcessing.cc
+++ src/IReceiveCallback/pyDataProcessing.cc
@@ -53,45 +53,46 @@ void export_DataProcessingMultipleConnections(py::module &m)
    py::class_<DataProcessingMultipleConnections, Network::IReceiveCallback>(m, "SDICOS::DataProcessingMultipleConnections");
    py::class_<PyDataProcessingMultipleConnections, DataProcessingMultipleConnections, Network::IReceiveCallback>(m, "DataProcessingMultipleConnections")
       .def(py::init<>())
-      .def("OnReceiveDicosFileError", py::overload_cast<const SDICOS::ErrorLog&, const SDICOS::Utils::SessionData&>
-                                      (&PyPublicDataProcessingMultipleConnections::OnReceiveDicosFileError),
-                                      py::arg("errorlog"), 
-                                      py::arg("sessiondata"))
 
-      .def("OnReceiveDicosFile", py::overload_cast<SDICOS::Utils::DicosData<SDICOS::AttributeManager> &, const SDICOS::ErrorLog &>
-                                      (&PyPublicDataProcessingMultipleConnections::OnReceiveDicosFile),
-                                      py::arg("manager"), 
-                                      py::arg("errorlog"))
+      .def("OnReceiveDicosFileError", 
+           [](PyPublicDataProcessingMultipleConnections &self, const SDICOS::ErrorLog &errorlog, const SDICOS::Utils::SessionData &sessiondata) {
+               self.OnReceiveDicosFileError(errorlog, sessiondata);
+           }, py::arg("errorlog"), py::arg("sessiondata"))
 
-      .def("OnReceiveDicosFile", py::overload_cast<SDICOS::Utils::DicosData<SDICOS::CT> &, const SDICOS::ErrorLog &>
-                                      (&PyPublicDataProcessingMultipleConnections::OnReceiveDicosFile),
-                                      py::arg("ct"), 
-                                      py::arg("errorlog"))
+      .def("OnReceiveDicosFile", 
+           [](PyPublicDataProcessingMultipleConnections &self, SDICOS::Utils::DicosData<SDICOS::AttributeManager> &manager, const SDICOS::ErrorLog &errorlog) {
+               self.OnReceiveDicosFile(manager, errorlog);
+           }, py::arg("manager"), py::arg("errorlog"))
 
-      .def("OnReceiveDicosFile", py::overload_cast<SDICOS::Utils::DicosData<SDICOS::DX> &, const SDICOS::ErrorLog &>
-                                      (&PyPublicDataProcessingMultipleConnections::OnReceiveDicosFile),
-                                      py::arg("dx"), 
-                                      py::arg("errorlog"))    
+      .def("OnReceiveDicosFile", 
+           [](PyPublicDataProcessingMultipleConnections &self, SDICOS::Utils::DicosData<SDICOS::CT> &ct, const SDICOS::ErrorLog &errorlog) {
+               self.OnReceiveDicosFile(ct, errorlog);
+           }, py::arg("ct"), py::arg("errorlog"))
 
-      .def("OnReceiveDicosFile", py::overload_cast<SDICOS::Utils::DicosData<SDICOS::AIT2D> &, const SDICOS::ErrorLog &>
-                                      (&PyPublicDataProcessingMultipleConnections::OnReceiveDicosFile),
-                                      py::arg("ait"), 
-                                      py::arg("errorlog"))
+      .def("OnReceiveDicosFile", 
+           [](PyPublicDataProcessingMultipleConnections &self, SDICOS::Utils::DicosData<SDICOS::DX> &dx, const SDICOS::ErrorLog &errorlog) {
+               self.OnReceiveDicosFile(dx, errorlog);
+           }, py::arg("dx"), py::arg("errorlog"))    
 
-      .def("OnReceiveDicosFile", py::overload_cast<SDICOS::Utils::DicosData<SDICOS::AIT3D> &, const SDICOS::ErrorLog &>
-                                      (&PyPublicDataProcessingMultipleConnections::OnReceiveDicosFile),
-                                      py::arg("ait"), 
-                                      py::arg("errorlog"))     
+      .def("OnReceiveDicosFile", 
+           [](PyPublicDataProcessingMultipleConnections &self, SDICOS::Utils::DicosData<SDICOS::AIT2D> &ait, const SDICOS::ErrorLog &errorlog) {
+               self.OnReceiveDicosFile(ait, errorlog);
+           }, py::arg("ait"), py::arg("errorlog"))
 
-      .def("OnReceiveDicosFile", py::overload_cast<SDICOS::Utils::DicosData<SDICOS::TDR> &, const SDICOS::ErrorLog &>
-                                      (&PyPublicDataProcessingMultipleConnections::OnReceiveDicosFile),
-                                      py::arg("tdr"), 
-                                      py::arg("errorlog"))    
+      .def("OnReceiveDicosFile", 
+           [](PyPublicDataProcessingMultipleConnections &self, SDICOS::Utils::DicosData<SDICOS::AIT3D> &ait, const SDICOS::ErrorLog &errorlog) {
+               self.OnReceiveDicosFile(ait, errorlog);
+           }, py::arg("ait"), py::arg("errorlog"))     
 
-      .def("OnReceiveDicosEcho", py::overload_cast<const SDICOS::Utils::SessionData &, const SDICOS::ErrorLog &>
-                                      (&PyPublicDataProcessingMultipleConnections::OnReceiveDicosEcho),
-                                      py::arg("sessiondata"), 
-                                      py::arg("errorlog"));                                                                                                                                                                                
+      .def("OnReceiveDicosFile", 
+           [](PyPublicDataProcessingMultipleConnections &self, SDICOS::Utils::DicosData<SDICOS::TDR> &tdr, const SDICOS::ErrorLog &errorlog) {
+               self.OnReceiveDicosFile(tdr, errorlog);
+           }, py::arg("tdr"), py::arg("errorlog"))    
+
+      .def("OnReceiveDicosEcho", 
+           [](PyPublicDataProcessingMultipleConnections &self, const SDICOS::Utils::SessionData &sessiondata, const SDICOS::ErrorLog &errorlog) {
+               self.OnReceiveDicosEcho(sessiondata, errorlog);
+           }, py::arg("sessiondata"), py::arg("errorlog"));
 }
 
 
@@ -140,38 +141,39 @@ void export_DataProcessingConnectionsSpecificClientApplications(py::module &m)
    py::class_<DataProcessingConnectionsSpecificClientApplications, Network::IReceiveCallback>(m, "SDICOS::DataProcessingConnectsSpecificClientApps");
    py::class_<PyDataProcessingConnectionsSpecificClientApplications, DataProcessingConnectionsSpecificClientApplications, Network::IReceiveCallback>(m, "DataProcessingConnectsSpecificClientApps")
       .def(py::init<>())
-      .def("OnReceiveDicosFileError", py::overload_cast<const SDICOS::ErrorLog&, const SDICOS::Utils::SessionData&>
-                                      (&PyPublicDataProcessingConnectionsSpecificClientApplications::OnReceiveDicosFileError),
-                                      py::arg("errorlog"), 
-                                      py::arg("sessiondata"))
 
-      .def("OnReceiveDicosFile", py::overload_cast<SDICOS::Utils::DicosData<SDICOS::AttributeManager> &, const SDICOS::ErrorLog &>
-                                      (&PyPublicDataProcessingConnectionsSpecificClientApplications::OnReceiveDicosFile),
-                                      py::arg("manager"), 
-                                      py::arg("errorlog"))
+      .def("OnReceiveDicosFileError", 
+           [](PyPublicDataProcessingConnectionsSpecificClientApplications &self, const SDICOS::ErrorLog &errorlog, const SDICOS::Utils::SessionData &sessiondata) {
+               self.OnReceiveDicosFileError(errorlog, sessiondata);
+           }, py::arg("errorlog"), py::arg("sessiondata"))
 
-      .def("OnReceiveDicosFile", py::overload_cast<SDICOS::Utils::DicosData<SDICOS::CT> &, const SDICOS::ErrorLog &>
-                                      (&PyPublicDataProcessingConnectionsSpecificClientApplications::OnReceiveDicosFile),
-                                      py::arg("ct"), 
-                                      py::arg("errorlog"))
+      .def("OnReceiveDicosFile", 
+           [](PyPublicDataProcessingConnectionsSpecificClientApplications &self, SDICOS::Utils::DicosData<SDICOS::AttributeManager> &manager, const SDICOS::ErrorLog &errorlog) {
+               self.OnReceiveDicosFile(manager, errorlog);
+           }, py::arg("manager"), py::arg("errorlog"))
 
-      .def("OnReceiveDicosFile", py::overload_cast<SDICOS::Utils::DicosData<SDICOS::DX> &, const SDICOS::ErrorLog &>
-                                      (&PyPublicDataProcessingConnectionsSpecificClientApplications::OnReceiveDicosFile),
-                                      py::arg("dx"), 
-                                      py::arg("errorlog"))    
+      .def("OnReceiveDicosFile", 
+           [](PyPublicDataProcessingConnectionsSpecificClientApplications &self, SDICOS::Utils::DicosData<SDICOS::CT> &ct, const SDICOS::ErrorLog &errorlog) {
+               self.OnReceiveDicosFile(ct, errorlog);
+           }, py::arg("ct"), py::arg("errorlog"))
 
-      .def("OnReceiveDicosFile", py::overload_cast<SDICOS::Utils::DicosData<SDICOS::AIT2D> &, const SDICOS::ErrorLog &>
-                                      (&PyPublicDataProcessingConnectionsSpecificClientApplications::OnReceiveDicosFile),
-                                      py::arg("ait"), 
-                                      py::arg("errorlog"))
+      .def("OnReceiveDicosFile", 
+           [](PyPublicDataProcessingConnectionsSpecificClientApplications &self, SDICOS::Utils::DicosData<SDICOS::DX> &dx, const SDICOS::ErrorLog &errorlog) {
+               self.OnReceiveDicosFile(dx, errorlog);
+           }, py::arg("dx"), py::arg("errorlog"))    
 
-      .def("OnReceiveDicosFile", py::overload_cast<SDICOS::Utils::DicosData<SDICOS::AIT3D> &, const SDICOS::ErrorLog &>
-                                      (&PyPublicDataProcessingConnectionsSpecificClientApplications::OnReceiveDicosFile),
-                                      py::arg("ait"), 
-                                      py::arg("errorlog"))     
+      .def("OnReceiveDicosFile", 
+           [](PyPublicDataProcessingConnectionsSpecificClientApplications &self, SDICOS::Utils::DicosData<SDICOS::AIT2D> &ait, const SDICOS::ErrorLog &errorlog) {
+               self.OnReceiveDicosFile(ait, errorlog);
+           }, py::arg("ait"), py::arg("errorlog"))
 
-      .def("OnReceiveDicosFile", py::overload_cast<SDICOS::Utils::DicosData<SDICOS::TDR> &, const SDICOS::ErrorLog &>
-                                      (&PyPublicDataProcessingConnectionsSpecificClientApplications::OnReceiveDicosFile),
-                                      py::arg("tdr"), 
-                                      py::arg("errorlog"));                                                                                                                                                                              
+      .def("OnReceiveDicosFile", 
+           [](PyPublicDataProcessingConnectionsSpecificClientApplications &self, SDICOS::Utils::DicosData<SDICOS::AIT3D> &ait, const SDICOS::ErrorLog &errorlog) {
+               self.OnReceiveDicosFile(ait, errorlog);
+           }, py::arg("ait"), py::arg("errorlog"))     
+
+      .def("OnReceiveDicosFile", 
+           [](PyPublicDataProcessingConnectionsSpecificClientApplications &self, SDICOS::Utils::DicosData<SDICOS::TDR> &tdr, const SDICOS::ErrorLog &errorlog) {
+               self.OnReceiveDicosFile(tdr, errorlog);
+           }, py::arg("tdr"), py::arg("errorlog"));
 }
